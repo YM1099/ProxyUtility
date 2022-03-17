ce97e53a94e3cab17fbd0697effb4394f4eb28e4
YM1099 <7499@pm.me>
2022-03-17 19:48:29 +0800

    bilibili

---
+++ Quantumult-X/rewrite/bilibili.js
@@ -0,0 +1,28 @@
+// 用于精简嗶哩嗶哩（港澳台版本）首页Tab、底栏按钮的脚本
+// ^https?://app\.bilibili\.com\/x\/resource\/show\/tab\/v2 url script-response-body https://raw.githubusercontent.com/YM1099/ProxyUtility/master/Quantumult-X/rewrite/bilibili.js
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+// 首页Tab，保留“直播、推薦、熱門”
+var count = 0;
+list = [731, 477, 478]
+for (const element of obj['data']['tab']) {
+    if (!list.includes(element['id'])) {
+        obj['data']['tab'].splice(count, 1);
+    }
+    count = count + 1;
+}
+
+// 底栏，移除“發燒影片”
+var count = 0;
+for (const element of obj['data']['bottom']) {
+    if (element['id'] === 736) {
+        obj['data']['bottom'].splice(count, 1);
+        break;
+    }
+    count = count + 1;
+}
+
+body = JSON.stringify(obj);
+$done(body);
